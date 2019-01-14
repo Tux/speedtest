40d5b2db78ab5ced3a14999752ddc578a7f24a01
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2019-01-14 08:58:01 +0100

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -72,7 +72,7 @@ configure_requires:
 test_requires:
     Test::More:          0
 test_recommends:
-    Test::More:          1.302141
+    Test::More:          1.302156
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/speedtest
